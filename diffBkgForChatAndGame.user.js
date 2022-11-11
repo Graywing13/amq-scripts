9a788df927154813543731b741316e4453fedacb
Graywing13 <30674558+Graywing13@users.noreply.github.com>
2022-11-11 14:28:17 -0800

    Add gradient chat colours

--- diffBkgForChatAndGame.user.js
+++ diffBkgForChatAndGame.user.js
@@ -5,20 +5,23 @@
 // @description  AMQ Diff Background For Chat and Game
 // @author       Graywing13
 // @match        https://animemusicquiz.com/
-// @downloadURL  https://github.com/Graywing13/amq-scripts/blob/main/diffBkgForChatAndGame.user.js
-// @updateURL    https://github.com/Graywing13/amq-scripts/blob/main/diffBkgForChatAndGame.user.js
 // ==/UserScript==
 
 // === [ SETTINGS ] ============================================
 
-const chatBgLink = "https://images.immediate.co.uk/production/volatile/sites/4/2018/08/iStock_13967830_XLARGE-90f249d.jpg"
+// GAME BKG SETTINGS. Make sure the image link ends in .jpg, .png, or an equivalent.
 const gameBgLink = "https://images.livemint.com/img/2022/08/01/1600x900/Cat-andriyko-podilnyk-RCfi7vgJjUY-unsplash_1659328989095_1659328998370_1659328998370.jpg"
+
+// CHAT SETTINGS. You can pick an image and two colours.
+const topChatColourHex = "rgba(228, 38, 38, 0.5)"; // Use the format rgba(r, g, b, a) where a is a number from 0 to 1.
+const bottomChatColourHex = "rgba(73, 4, 138, 0.5)";
+const chatBgLink = "https://images.immediate.co.uk/production/volatile/sites/4/2018/08/iStock_13967830_XLARGE-90f249d.jpg"
 
 // =============================================================
 
 let msgContainer = document.getElementById("gcMessageContainer");
 if (msgContainer !== undefined) {
-    msgContainer.style.backgroundImage=`url(${chatBgLink})`;
+    msgContainer.style.backgroundImage=`linear-gradient(to bottom, ${topChatColourHex}, ${bottomChatColourHex}), url(${chatBgLink})`;
     msgContainer.style.backgroundSize="cover";
     msgContainer.style.backgroundPosition="center";
 }
