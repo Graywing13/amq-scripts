680b4fbbbccba7e29f65d77d1729ec3e7fccb4dc
Graywing13 <30674558+Graywing13@users.noreply.github.com>
2022-11-11 18:17:45 -0800

    Set "" as the default game background url
    People are running into alignment issues. By setting the game URL to none, it only uses elodie's background so u can avoid them.

--- diffBkgForChatAndGame.user.js
+++ diffBkgForChatAndGame.user.js
@@ -10,11 +10,12 @@
 // === [ SETTINGS ] ============================================
 
 // GAME BKG SETTINGS. Make sure the image link ends in .jpg, .png, or an equivalent.
-const gameBgLink = "https://images.livemint.com/img/2022/08/01/1600x900/Cat-andriyko-podilnyk-RCfi7vgJjUY-unsplash_1659328989095_1659328998370_1659328998370.jpg"
+// If you are facing misalignment issues with Elodie's script, your best bet is to set gameBgLink to "" (an empty string). This will only use Elodie's background.
+const gameBgLink = "";
 
 // CHAT SETTINGS. You can pick an image and two colours.
-const topChatColourHex = "rgba(0, 0, 0, 0.5)"; // Use the format rgba(r, g, b, a) where a is a number from 0 to 1.
-const bottomChatColourHex = "rgba(0, 0, 0, 0.5)";
+const topChatColourHex = "rgba(228, 38, 38, 0.5)"; // Use the format rgba(r, g, b, a) where a is a number from 0 to 1.
+const bottomChatColourHex = "rgba(73, 4, 138, 0.5)";
 const chatBgLink = "https://images.immediate.co.uk/production/volatile/sites/4/2018/08/iStock_13967830_XLARGE-90f249d.jpg"
 
 // =============================================================
