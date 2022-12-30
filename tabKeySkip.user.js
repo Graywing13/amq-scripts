ff67622981935c7bbcb5ced2f5484399c6f98cbb
dashdot <30674558+Graywing13@users.noreply.github.com>
2022-12-30 15:39:46 -0800

    Add tabKeySkip and its documentation

---
+++ tabKeySkip.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         AMQ Tab Key to Skip
+// @namespace    https://github.com/Graywing13
+// @version      1.0
+// @description  Lets AMQ songs be skipped using the tab key while playing an AMQ game.
+// @author       Graywing13
+// @match        https://animemusicquiz.com/*
+// @downloadURL  https://github.com/Graywing13/amq-scripts/blob/main/tabKeySkip.user.js
+// @updateURL    https://github.com/Graywing13/amq-scripts/blob/main/tabKeySkip.user.js
+// ==/UserScript==
+
+// credits to TheJoseph98 for this code block
+if (document.getElementById("startPage")) return;
+let loadInterval = setInterval(() => {
+  if (document.getElementById("loadingScreen").classList.contains("hidden")) {
+    clearInterval(loadInterval);
+  }
+}, 500);
+
+// quiz skip with tab key
+document.addEventListener("keyup", function (event) {
+  if (event.key === "Tab") {
+    event.preventDefault();
+    quiz.skipClicked();
+  }
+});
