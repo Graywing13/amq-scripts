956d0304075d64c6bb500353088c9b29a7221f5c
Noreen Chan <30674558+Graywing13@users.noreply.github.com>
2023-08-05 05:31:49 -0700

    Fix typo

--- diffBkgForChatAndGame.user.js
+++ diffBkgForChatAndGame.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AMQ Different Bkg For Chat/Game
 // @namespace    http://tampermonkey.net/
-// @version      1.2.2
+// @version      1.2.3
 // @description  AMQ Diff Background For Chat and Game
 // @author       Graywing13
 // @match        https://animemusicquiz.com/*
@@ -11,7 +11,7 @@
 
 // === [ SETTINGS ] ============================================
 
-if (!document.getElementById("loginPage")) return;
+if (document.getElementById("loginPage")) return;
 
 // GAME BKG SETTINGS. Make sure the image link ends in .jpg, .png, or an equivalent.
 const gameBgLink = "https://images.livemint.com/img/2022/08/01/1600x900/Cat-andriyko-podilnyk-RCfi7vgJjUY-unsplash_1659328989095_1659328998370_1659328998370.jpg"
