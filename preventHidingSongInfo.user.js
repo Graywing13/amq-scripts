d5679c2695e811ecfed42537d99500625feccb00
graywing13 <30674558+Graywing13@users.noreply.github.com>
2025-06-05 21:41:38 -0700

    feat: preventHidingSongInfo displays last anime title
    * Hover to see full title
    * Click to open song info

--- preventHidingSongInfo.user.js
+++ preventHidingSongInfo.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         AMQ Prevent Hiding Song Info
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.3
 // @description  Show previous song's info during guessing phase
 // @author       Graywing13
 // @match        https://animemusicquiz.com/*
-// @downloadURL  https://github.com/Graywing13/amq-scripts/blob/main/preventHidingSongInfo.user.js
-// @updateURL    https://github.com/Graywing13/amq-scripts/blob/main/preventHidingSongInfo.user.js
+// @downloadURL  https://github.com/Graywing13/amq-scripts/raw/main/preventHidingSongInfo.user.js
+// @updateURL    https://github.com/Graywing13/amq-scripts/raw/main/preventHidingSongInfo.user.js
 // ==/UserScript==
 
 if (typeof Listener === "undefined") return;
@@ -18,11 +18,54 @@ const loadInterval = setInterval(() => {
 }, 500);
 
 function setup() {
+    setupDom()
+    setupListener()
+}
+
+function setupDom() {
     $('#qpInfoHider').css({
         "display": "none"
     })
-    $('#qpSongInfoContainer h3').text("Last Song's Info")
-    $('#qpSongName').text("n/a")
-    $('#qpSongArtist').text("n/a")
-    $('#qpSongType').text("n/a")
+    $('#qpSongName').text("-")
+    $('#qpSongArtist').text("-")
+    $('#qpSongType').text("-")
+
+    const header = $('#qpSongInfoContainer h3')
+    header.text("Prev: n/a")
+    header.css({
+        'text-overflow': 'ellipsis',
+        'overflow': 'hidden',
+        'height': '1.3em',
+        'white-space': 'nowrap',
+        'padding': '0 8px'
+    })
+    header.popover({
+        placement: 'bottom',
+        content: 'No previous song yet',
+        trigger: 'hover',
+        container: 'body',
+        animation: false
+    })
+}
+
+function setupListener() {
+    const answerResultsListener = new Listener("answer results", (result) => {
+        const animeName = getAnimeName(result)
+        const header = $('#qpSongInfoContainer h3')
+        header.text("Prev: " + animeName)
+        header.attr('data-content', animeName)
+        header.addClass('aicTrigger')
+        header.click(() => {
+            songHistoryWindow?.currentGameTab?.table?.rows?.slice(-1)?.[0]?.$body?.[0]?.click()
+        })
+    })
+    answerResultsListener.bindListener()
+}
+
+function getAnimeName(result) {
+    if (options?.useRomajiNames) {
+        return result.songInfo.animeNames["romaji"]
+    } else {
+        return result.songInfo.animeNames["english"]
+    }
 }
